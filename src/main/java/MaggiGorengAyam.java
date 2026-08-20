26baca478694b238759da763aea83b9a29d6bcc0
Shawn Kok <shawnkokxk@gmail.com>
2026-08-20 15:01:59 +0800

    remove Duke.java, add MaggiGorengAyam.java and ammend README.md

---
+++ src/main/java/MaggiGorengAyam.java
@@ -0,0 +1,16 @@
+public class MaggiGorengAyam {
+    public static void main(String[] args) {
+        String banner = "____________________________________________________________\n" +
+                "  __  __  _____    _    \n" +
+                " |  \\/  |/ ____|  / \\   \n" +
+                " | \\  / ||   __  / _ \\  \n" +
+                " | |\\/| ||  |_ |/ ___ \\ \n" +
+                " |_|  |_|\\_____/_/   \\_\\\n" +
+                "Hello! I'm Maggi Goreng Ayam.\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(banner);
+    }
+}

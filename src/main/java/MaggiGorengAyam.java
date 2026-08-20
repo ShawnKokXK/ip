dfe8d8e3cddfbf425f99160a843955f278a65084
Shawn Kok <shawnkokxk@gmail.com>
2026-08-20 15:26:41 +0800

    update echo

--- src/main/java/MaggiGorengAyam.java
+++ src/main/java/MaggiGorengAyam.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class MaggiGorengAyam {
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String banner = "____________________________________________________________\n" +
+        String banner = LINE + "\n" +
                 "  __  __  _____    _    \n" +
                 " |  \\/  |/ ____|  / \\   \n" +
                 " | \\  / ||   __  / _ \\  \n" +
@@ -8,9 +12,22 @@ public class MaggiGorengAyam {
                 " |_|  |_|\\_____/_/   \\_\\\n" +
                 "Hello! I'm Maggi Goreng Ayam.\n" +
                 "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+                LINE;
         System.out.println(banner);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(LINE);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+            System.out.println(LINE);
+            System.out.println(" " + command);
+            System.out.println(LINE);
+        }
+        scanner.close();
     }
 }

e96a14bc32908ddfe6d992f969c4477eea0198a3
Shawn Kok <shawnkokxk@gmail.com>
2026-08-20 15:48:39 +0800

    mark feature

--- src/main/java/MaggiGorengAyam.java
+++ src/main/java/MaggiGorengAyam.java
@@ -16,6 +16,7 @@ public class MaggiGorengAyam {
         System.out.println(banner);
 
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -29,9 +30,20 @@ public class MaggiGorengAyam {
             }
             if (command.equals("list")) {
                 System.out.println(LINE);
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    String mark = isDone[i] ? "X" : " ";
+                    System.out.println(" " + (i + 1) + ".[" + mark + "] " + tasks[i]);
                 }
+                System.out.println(LINE);
+                continue;
+            }
+            if (command.startsWith("mark ")) {
+                int index = Integer.parseInt(command.substring(5).trim()) - 1;
+                isDone[index] = true;
+                System.out.println(LINE);
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   [X] " + tasks[index]);
                 System.out.println(LINE);
                 continue;
             }
